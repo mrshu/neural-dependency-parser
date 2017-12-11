6f4337acd6f747ae951d29a94c3fc650751b59b6
mr.Shu <mr@shu.io>
2017-12-11 21:54:25 +0100

    update: Add implemented model
    * Added implemented model
    
    * Added some more helper functions
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- data_import.py
+++ data_import.py
@@ -1,48 +1,101 @@
 import re
 from collections import Counter
 from collections import defaultdict
+import numpy as np
 
-file = open('data/en-ud-train.conllu', 'r', encoding="utf8")
-pos_tags = []
-vocabulary = []
+def read_voc_pos_tags_from_conllu_file(filename):
+    file = open(filename, 'r', encoding="utf8")
+    pos_tags = []
+    vocabulary = []
+    sentences = []
 
-for line in file:
-    if line.startswith('#'):
-        continue
-    if line != '\n':
-        line_split = line.split('\t')
-        # remove sentences which start with integer index with hyphens
-        if re.match("\d+[-]\d+", line_split[0]):
-            file.remove(line)
-        pos_tags.append(line_split[4])
-        vocabulary.append(line_split[1])
+    text = file.read()
 
-pos_tags = list(set(pos_tags))
+    for sentence in text.split('\n\n'):
+        s = {}
+        w2i = defaultdict(lambda: len(w2i))
+        w2i['0'] = 0
+        for line in sentence.split('\n'):
+            if line.startswith('#'):
+                continue
+            if line and line != '\n':
+                line_split = line.split('\t')
+                # remove sentences which start with integer index with hyphens
+                if re.match("\d+[-]\d+", line_split[0]):
+                    file.remove(line)
+                id = w2i[line_split[0]]
 
-voc_counter = Counter(vocabulary)
+                s[id] = ([line_split[1].lower(),
+                          line_split[4],
+                          line_split[6]])
 
-#replace words that occur once with <unk>
-for word in vocabulary:
-    if voc_counter[word] < 2:
-        vocabulary.remove(word)
-        vocabulary.append('<unk>')
+                pos_tags.append(line_split[4])
+                vocabulary.append(line_split[1].lower())
 
-voc_counter = Counter(vocabulary)
+        M = np.zeros((len(s) + 1, len(s) + 1))
+        for i, w in enumerate(s.keys()):
+            if s[w][2] == '_':
+                continue
+            M[w2i[s[w][2]]][i+1] = 1
+        M[0, 0] = 1
+        if s:
+            sentences.append([s, M])
+    return vocabulary, pos_tags, sentences
 
-w2i = defaultdict(lambda: len(w2i))
-t2i = defaultdict(lambda: len(t2i))
-UNK = w2i["<unk>"]
-w2i = defaultdict(lambda: UNK, w2i)
+def read_conllu_file(filename):
 
-for index, word in enumerate(voc_counter):
-    w2i[word] = index
+    vocabulary, pos_tags, sentences = read_voc_pos_tags_from_conllu_file(filename)
 
-i2w = {v: k for k, v in w2i.items()}
+    vocabulary = set(vocabulary)
+    pos_tags = list(set(pos_tags))
+    voc_counter = Counter(vocabulary)
 
-for index, tag in enumerate(pos_tags):
-    t2i[tag] = index
+    filtered_vocabulary = set()
 
-i2t = {v: k for k, v in t2i.items()}
+    # replace words that occur once with <unk>
+    for word in vocabulary:
+        if voc_counter[word] > 2:
+            filtered_vocabulary.add(word)
+        else:
+            filtered_vocabulary.add('<unk>')
 
-l2i = {1: 'root', 2: 'dep', 3: 'aux', 4: 'auxpass', 5: 'cop', 6: 'arg', 7: 'agent', 8: 'comp', 9: 'acomp', 10: 'ccomp', 11: 'xcomp', 12: 'obj', 13: 'dobj', 14: 'iobj', 15: 'pobj', 16: 'subj', 17: 'nsubj', 18: 'nsubjpass', 19: 'csubj', 20: 'csubjpass', 21: 'cc', 22: 'conj', 23: 'expl', 24: 'mod', 25: 'amod', 26: 'appos', 27: 'advcl', 28: 'det', 29: 'predet', 30: 'preconj', 31: 'vmod', 32: 'mwe', 33: 'mark', 34: 'advmod', 35: 'neg', 36: 'rcmod', 37: 'quantmod', 38: 'nn', 39: 'npadvmod', 40: 'tmod', 41: 'num', 42: 'number', 43: 'prep', 44: 'poss', 45: 'possessive', 46: 'prt', 47: 'parataxis', 48: 'goeswith', 49: 'punct', 50: 'ref', 51: 'sdep', 52: 'xsubj', 53: 'partmod', 54: 'abbrev', 55: 'attr', 56: 'complm', 57: 'discourse', 58: 'infmod', 59: 'purpcl', 60: 'rel', 61: 'pcomp'}
-i2l = {v: k for k, v in l2i.items()}
+    voc_counter = Counter(vocabulary)
+
+    w2i = defaultdict(lambda: len(w2i))
+    t2i = defaultdict(lambda: len(t2i))
+    UNK = w2i["<unk>"]
+    w2i = defaultdict(lambda: UNK, w2i)
+
+    for index, word in enumerate(voc_counter):
+        w2i[word] = index
+
+    i2w = {v: k for k, v in w2i.items()}
+
+    for index, tag in enumerate(pos_tags):
+        t2i[tag] = index
+
+    i2t = {v: k for k, v in t2i.items()}
+
+    l2i = {1: 'root', 2: 'dep', 3: 'aux', 4: 'auxpass', 5: 'cop', 6: 'arg', 7:
+           'agent', 8: 'comp', 9: 'acomp', 10: 'ccomp', 11: 'xcomp', 12: 'obj',
+           13: 'dobj', 14: 'iobj', 15: 'pobj', 16: 'subj', 17: 'nsubj', 18:
+           'nsubjpass', 19: 'csubj', 20: 'csubjpass', 21: 'cc', 22: 'conj', 23:
+           'expl', 24: 'mod', 25: 'amod', 26: 'appos', 27: 'advcl', 28: 'det',
+           29: 'predet', 30: 'preconj', 31: 'vmod', 32: 'mwe', 33: 'mark', 34:
+           'advmod', 35: 'neg', 36: 'rcmod', 37: 'quantmod', 38: 'nn', 39:
+           'npadvmod', 40: 'tmod', 41: 'num', 42: 'number', 43: 'prep', 44:
+           'poss', 45: 'possessive', 46: 'prt', 47: 'parataxis', 48:
+           'goeswith', 49: 'punct', 50: 'ref', 51: 'sdep', 52: 'xsubj', 53:
+           'partmod', 54: 'abbrev', 55: 'attr', 56: 'complm', 57: 'discourse',
+           58: 'infmod', 59: 'purpcl', 60: 'rel', 61: 'pcomp'}
+
+    i2l = {v: k for k, v in l2i.items()}
+
+    index_sentences = []
+    for (sentence, _) in sentences:
+        s = []
+        for k, v in sentence.items():
+            s.append((w2i[v[0]], t2i[v[1]]))
+        index_sentences.append(s)
+
+    return w2i, i2w, t2i, i2t, l2i, i2l, sentences, index_sentences
--- model.py
+++ model.py
@@ -1,27 +1,94 @@
 import torch
 import torch.nn as nn
+import torch.optim as optim
+from itertools import product
 from torch.autograd import Variable
+import numpy as np
 
 class DepParser(nn.Module):
-    def __init__(self, word_embedding, pos_embedding, d_embed, hidden_size):
+    def __init__(self, word_voc_size, pos_embedding_len, d_embed, hidden_size):
         super(DepParser, self).__init__()
         self.hidden_size = hidden_size
         self.d_embed = d_embed
 
-        self.w_embedding = nn.Embedding(word_embedding.size(0),
-                                        word_embedding.size(1))
-        self.w_embedding.weight = nn.Parameter(word_embedding)
+        self.w_embedding = nn.Embedding(word_voc_size, d_embed)
+        self.pos_embedding = nn.Embedding(pos_embedding_len, d_embed)
 
-        self.pos_embedding = nn.Embedding(pos_embedding.size(0),
-                                          pos_embedding.size(1))
-        self.pos_embedding.weight = nn.Parameter(pos_embedding)
+        self.lstm = nn.LSTM(2 * d_embed, hidden_size, 1)
+
+        self.fc1 = torch.nn.Linear(2 * hidden_size, hidden_size)
+        self.tanh = nn.Tanh()
+        self.fc2 = torch.nn.Linear(hidden_size, 1)
+
+        self.root = Variable(torch.zeros((1, 1, hidden_size)),
+                             requires_grad=False)
+
+    def forward(self, words, pos):
+        e_w = self.w_embedding(words)
+        e_p = self.pos_embedding(pos)
+
+        input = torch.cat((e_w, e_p), 1).view(len(words), 1, self.d_embed * 2)
+
+        output, (hidden, cell) = self.lstm(input)
+
+        M = Variable(torch.zeros(len(words) + 1, len(words) + 1))
+
+        # Concatentate the root vector
+        output = torch.cat((self.root, output), 0)
+
+        for w1, w2 in product(list(range(len(words) + 1)), repeat=2):
+            arc = torch.cat((output[w1], output[w2]), 1)
+
+            out = self.fc1(arc)
+            out = self.tanh(out)
+            M[w1, w2] = self.fc2(out)
+
+        return M
 
 
-        self.lstm = nn.LSTM(d_embed, hidden_size, 1)
+if __name__ == "__main__":
+    # from data_import import read_voc_pos_tags_from_conllu_file
+    # voc, pos, s = read_voc_pos_tags_from_conllu_file('./en-ud-dev.conllu.txt')
 
-    def forward(self, words, pos, hidden, cell):
-        e_w = self.embedding(words).view(1, 1, -1)
-        e_p = self.embedding(pos).view(1, 1, -1)
-        output = torch.cat((e_w, e_p), 0)
-        output, hidden, cell = self.lstm(output, (hidden, cell))
-        return output, hidden, cell
+    from data_import import read_conllu_file
+    w2i, i2w, t2i, i2t, l2i, i2l, sentences, index_sentences = read_conllu_file('./en-ud-dev.conllu.txt')
+
+    model = DepParser(len(w2i), len(t2i), 30, 50)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    for i in range(len(sentences)):
+        s, M = sentences[i]
+        sentence = index_sentences[i]
+
+        words = list(map(lambda x: x[0], sentence))
+        pos = list(map(lambda x: x[1], sentence))
+
+        rev_words = list(map(lambda x: i2w[x], words))
+        rev_pos = list(map(lambda x: i2t[x], pos))
+
+        # print(words, pos)
+        # print(rev_words, rev_pos)
+
+        words = Variable(torch.LongTensor(words))
+        pos = Variable(torch.LongTensor(pos))
+
+        optimizer.zero_grad()
+
+        out_M = model(words, pos)
+        t_out_M = torch.t(out_M)
+
+        if i > 0 and i % 50 == 0:
+            s = nn.Softmax()
+            print(torch.t(s(t_out_M)))
+            print(M)
+
+        np_targets = np.argmax(M, axis=0)
+        targets = Variable(torch.from_numpy(np_targets), requires_grad=False)
+
+        loss = criterion(t_out_M, targets)
+        print('{} loss: {}'.format(i, loss.data[0]))
+        loss.backward()
+        optimizer.step()
+
+
